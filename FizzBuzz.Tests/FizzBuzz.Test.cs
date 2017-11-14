37c7783913ef667ee7b574c2cad1e448864e7254
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-11-14 20:55:53 +0700

    Refactor

--- FizzBuzz.Tests/FizzBuzz.Test.cs
+++ FizzBuzz.Tests/FizzBuzz.Test.cs
@@ -1,53 +1,45 @@
 using System;
-using Xunit;
 using FizzBuzz;
+using Xunit;
 
-namespace FizzBuzz.Tests
-{
-    public class FizzBuzzTest
-    {
+namespace FizzBuzz.Tests {
+    public class FizzBuzzTest {
         private FizzBuzz fizzbuzz;
 
-        public FizzBuzzTest()
-        {
-            this.fizzbuzz = new FizzBuzz();
+        public FizzBuzzTest () {
+            this.fizzbuzz = new FizzBuzz ();
         }
 
         [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        public void TestShouldBeTheSameNumberWhenGiveANumberNotDivisibleBy3And5(int v)
-        {
-            var result = fizzbuzz.Say(v);
-            Assert.Equal(result, v.ToString());
+        [InlineData (1)]
+        [InlineData (2)]
+        public void TestShouldBeTheSameNumberWhenGiveANumberNotDivisibleBy3And5 (int v) {
+            var result = fizzbuzz.Say (v);
+            Assert.Equal (v.ToString (), result);
         }
 
         [Theory]
-        [InlineData(3)]
-        [InlineData(6)]
-        public void TestShouldBeFizzWhenGiveValueDivisibleBy3(int v)
-        {
-            var result = this.fizzbuzz.Say(v);
-            Assert.Equal(result, "Fizz");
+        [InlineData (3)]
+        [InlineData (6)]
+        public void TestShouldBeFizzWhenGiveValueDivisibleBy3 (int v) {
+            var result = this.fizzbuzz.Say (v);
+            Assert.Equal ("Fizz", result);
         }
 
         [Theory]
-        [InlineData(5)]
-        [InlineData(10)]
-        public void TestShouldBeBuzzWhenGiveValueDivisibleBy5(int v)
-        {
-            var result = this.fizzbuzz.Say(v);
-            Assert.Equal(result, "Buzz");
+        [InlineData (5)]
+        [InlineData (10)]
+        public void TestShouldBeBuzzWhenGiveValueDivisibleBy5 (int v) {
+            var result = this.fizzbuzz.Say (v);
+            Assert.Equal ("Buzz", result);
         }
 
         [Theory]
-        [InlineData(15)]
-        [InlineData(30)]
-        public void TestShouldBeFizzBuzzWhenGiveValueDivisibleBy15(int v)
-        {
-            var fizzbuzz = new FizzBuzz();
-            var result = this.fizzbuzz.Say(v);
-            Assert.Equal(result, "FizzBuzz");
+        [InlineData (15)]
+        [InlineData (30)]
+        public void TestShouldBeFizzBuzzWhenGiveValueDivisibleBy15 (int v) {
+            var result = this.fizzbuzz.Say (v);
+            Assert.Equal ("FizzBuzz", result);
         }
     }
 }
